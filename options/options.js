d14889dc6c17aa977e03cdf6719cab86477364ec
Crowdbotics <team@crowdbotics.com>
2024-02-05 16:44:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ecommdigmarkapp-45922",
+  url: "https://ecommdigmarkapp-45922.botics.co",
+  api: "https://ecommdigmarkapp-45922.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
